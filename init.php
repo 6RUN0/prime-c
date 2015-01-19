ce9bcf48e0fe8baf7121c8460750a9bb9ea76167
botis_t <boris@talovikov.ru>
2015-01-19 15:42:36 +0500

    support favicon.ico

--- init.php
+++ init.php
@@ -11,3 +11,15 @@ $themeInfo = array(
   'name' => 'Prime (Caldari Version)',
   'info'=> 'By Vecati',
 );
+
+class PrimeC
+{
+  public static function AddHeader(&$page)
+  {
+    if (file_exists(__DIR__ . '/favicon.ico')) {
+      $page->addHeader('<link rel="icon" type="image/vnd.microsoft.icon" href="' . THEME_URL . '/favicon.ico' . '" />');
+    }
+  }
+}
+
+event::register('page_assembleheader', 'PrimeC::AddHeader');
